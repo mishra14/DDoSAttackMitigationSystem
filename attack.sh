a3396f1bc3d77ffe44f04a47ed13b9200cbd76ce
Simon Wimmer <wimmers@seas.upenn.edu>
2015-04-26 15:54:19 -0400

    fix attack cmds

--- attack.sh
+++ attack.sh
@@ -4,8 +4,8 @@ BBh1 hping3 --flood --udp AAh1 &
 BBh1 hping3 --flood --udp AAh2 &
 BAh1 hping3 --flood --udp AAh1 &
 BAh1 hping3 --flood --udp AAh2 &
-BBh1 hping3 --flood --udp AAh1 &
-BBh1 hping3 --flood --udp AAh2 &
-BAh1 hping3 --flood --udp AAh1 &
-BAh1 hping3 --flood --udp AAh2 & 
+BBh2 hping3 --flood --udp AAh1 &
+BBh2 hping3 --flood --udp AAh2 &
+BAh2 hping3 --flood --udp AAh1 &
+BAh2 hping3 --flood --udp AAh2 & 
 
